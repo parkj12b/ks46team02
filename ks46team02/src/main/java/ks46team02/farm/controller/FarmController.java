eb0f6494002a228b0ce678af698273490d6d6cde
Jacob P. <p315315@gmail.com>
2023-04-24 01:08:14 +0900

    feat : add tinyMCE to vendors, add contract content modify

--- ks46team02/src/main/java/ks46team02/farm/controller/FarmController.java
+++ ks46team02/src/main/java/ks46team02/farm/controller/FarmController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -211,5 +213,12 @@ public class FarmController {
 		model.addAttribute("mmContractInfo",mmContractInfo);
 		
 		return "farm/mmContractModify";
+	}
+	
+	@PostMapping("/mmContractModifyAction")
+	public String setMMContractModify(MMContractInfo contractInfo) {
+		
+		log.info("{}", contractInfo);
+		return "redirect:/farm/myMentorMenteeContract";
 	}
 }
