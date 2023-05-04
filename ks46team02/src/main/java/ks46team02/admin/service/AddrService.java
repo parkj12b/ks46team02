ad9fcf31b121eaee1f6837255451979896e96370
ECS <ECS@DESKTOP-048HU3O>
2023-05-04 17:14:04 +0900

    2023-05-04 cbj

--- ks46team02/src/main/java/ks46team02/admin/service/AddrService.java
+++ ks46team02/src/main/java/ks46team02/admin/service/AddrService.java
@@ -7,21 +7,27 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ks46team02.admin.mapper.AddrMapper;
 import ks46team02.common.dto.Addr;
-import ks46team02.common.dto.Member;
+import ks46team02.common.mapper.MainMapper;
 @Service
 @Transactional
 public class AddrService {
 	
 private final AddrMapper addrMapper;
+private final MainMapper mainMapper;
 	
-	public AddrService(AddrMapper addrMapper) {
+	public AddrService( AddrMapper addrMapper
+					   ,MainMapper mainMapper) {
 		this.addrMapper = addrMapper;
-
+		this.mainMapper = mainMapper;
 	}
 	/* 배송지 등록 */
 	public int addAddr(Addr addr) {
-		int result = addrMapper.addAddr(addr);
-		return result;
+		 String column = "shipping_addr_code";
+	     String table = "addr_manage";
+	     String addrCode = mainMapper.autoIncrement(table, column);
+	     addr.setAddrCode(addrCode);
+	     int result = addrMapper.addAddr(addr);
+	     return result;
 	}
 	/* 배송지 조회 */
 	public List<Addr> getAddrList(){
@@ -32,5 +38,10 @@ private final AddrMapper addrMapper;
 	public Addr getAddrInfoById(String addrCode) {
 		Addr adddrInfo = addrMapper.getAddrInfoById(addrCode);
 		return adddrInfo;
+	}
+	/* 특정 회원 배송지 조회 */
+	public List<Addr> getAddrInfoByMemberId(String memberId) {
+		List<Addr> AddrList = addrMapper.getAddrMemberList(memberId);
+		return AddrList;
 	}
 	}
