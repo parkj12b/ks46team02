c4c00444f76fffdfc47b95e13f9596d5bd4b5074
kimmy427 <a01091555257@gmail.com>
2023-05-12 16:19:04 +0900

    [2023-05-12] 16:18 김유진 addFeed 완료

--- ks46team02/src/main/java/ks46team02/farm/mapper/FarmMapper.java
+++ ks46team02/src/main/java/ks46team02/farm/mapper/FarmMapper.java
@@ -16,6 +16,8 @@ import ks46team02.farm.dto.Production;
 @Mapper
 public interface FarmMapper {
 
+    // 먹이 급여 등록 //
+    public void addFeed(Feed feed);
     // 생산량 그래프 //
     public List<Production> getProductionGraph(String farmCode);
     // 먹이 급여 그래프 //
